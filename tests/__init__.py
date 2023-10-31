fbca7407d317d87e9777b8e3407a99e77dd40c11
timothy-r <tim.rodger@gmail.com>
2023-10-31 22:08:29 +0000

    Add fs director test to module

--- tests/__init__.py
+++ tests/__init__.py
@@ -6,6 +6,7 @@ import unittest
 from tests.index_page_test import IndexPageTest
 from tests.leaf_page_test import LeftPageTest
 from tests.template_builder_test import TemplateBuilderTest
+from tests.file_system_source_director_test import FileSystemSourceDirectorTest
 
 if __name__ == '__main__':
     runner = unittest.TextTestRunner()
